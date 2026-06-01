752547f36fc78d29abcce519fa09eb6bc9c6e474
Kartik-01-10 <hubcoding460@gmail.com>
2026-06-01 08:59:50 +0530

    updated

--- Week_1/Day_1/ques_1.cpp
+++ Week_1/Day_1/ques_1.cpp
@@ -4,14 +4,12 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int a = 0 ;
-        int b = n-1;
-        while(a<b){
-            if (nums[a]+nums[b]==target) return {a,b};
-            else if (nums[a]+nums[b]>target) b--;
-            else if (nums[a]+nums[b]<target) a++;
-        }
-        return {};
-    }
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<n;j++){
+                if ((nums[i]+nums[j])==target )
+                return {i,j};
+            }
+        }return {};
+
+    } 
 };
